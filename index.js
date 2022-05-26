ffcc0e81f585e6aaf01e991d4bb564b315de109b
Soyeb <tssoyeb@gmail.com>
2022-05-26 11:53:58 +0600

    get all orders

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ async function run() {
     const productCollection = client.db('computer_ingredient').collection('products');
     const orderCollection = client.db('computer_ingredient').collection('order');
     const userCollection = client.db('computer_ingredient').collection('user');
+    const paymentCollection = client.db('computer_ingredient').collection('payment')
 
 
     const verifyAdmin = async (req, res, next) => {
@@ -77,29 +78,29 @@ async function run() {
     app.get('/user/:email', async (req, res) => {
       const email = req.params.email;
       console.log(email);
-      const query = { email:email };
+      const query = { email: email };
       const user = await userCollection.findOne(query);
       res.send(user);
     })
 
-    app.get('/admin/:email', async(req, res) =>{
+    app.get('/admin/:email', async (req, res) => {
       const email = req.params.email;
-      const user = await userCollection.findOne({email: email});
+      const user = await userCollection.findOne({ email: email });
       const isAdmin = user.role === 'admin';
-      res.send({admin: isAdmin})
+      res.send({ admin: isAdmin })
     })
 
-    app.put('/user/admin/:email', verifyJWT,verifyAdmin, async (req, res) => {
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       // const requester = req.decoded.email;
       // const requesterAccount = await userCollection.findOne({ email: requester });
       // if (requesterAccount.role === 'admin') {
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: 'admin' },
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
       // } else {
       //   res.status(403).send({ message: 'forbidden' });
       // }
@@ -114,7 +115,7 @@ async function run() {
       const filter = { email: email }
       const options = { upsert: true }
       const updateDoc = {
-        $set:user
+        $set: user
       }
       const result = await userCollection.updateOne(filter, updateDoc, options)
       const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30d' })
@@ -135,24 +136,49 @@ async function run() {
       }
 
     })
+
+    app.patch('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId
+        }
+      }
+
+      const result = await paymentCollection.insertOne(payment);
+      const updatedordering = await orderCollection.updateOne(filter, updatedDoc);
+      res.send(updatedordering);
+    })
+
     app.get('/orders/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const order = await orderCollection.findOne(query);
       res.send(order);
-  })
-   //stripe payment intent
-   app.post('/create-payment-intent', async (req, res) => {
-    const order = req.body;
-    const price = order.total_price;
-    const amount = price * 100;
-    const paymentIntent = await stripe.paymentIntents.create({
+    })
+
+    //all orders
+    app.get('/orders', async (req, res) => {
+      const query = {};
+      const orders = await orderCollection.find(query).toArray();
+      res.send(orders);
+    });
+
+    //stripe payment intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const order = req.body;
+      const price = order.total_price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: 'usd',
         payment_method_types: ['card']
-    });
-    res.send({ clientSecret: paymentIntent.client_secret });
-})
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    })
 
 
     app.post('/orders', async (req, res) => {
